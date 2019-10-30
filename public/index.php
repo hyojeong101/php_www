9afe8156f79de0a6ed84a2709c9d81a4bc76f674
hyojeong101 <aaa8132@naver.com>
2019-10-30 15:29:13 +0900

    sql desc

--- public/index.php
+++ public/index.php
@@ -3,18 +3,19 @@ $config = include "../dbconf.php";
 require "../Loading.php";
 $uri = $_SERVER['REQUEST_URI'];
 $uris = explode("/",$uri);//파란책
-print_r($uris);
+//print_r($uris);
 
 $db = new \Module\Database\Database( $config );
 
-if(isset($uri[1])&& $uris[1]){
+if(isset($uris[1])&& $uris[1]){
     //컨트롤러 실행
-    echo $uri[1]."컨트롤러 실행";
+   // echo $uri[1]."컨트롤러 실행";
     $controllerName = "\App\Controller\\".ucfirst($uris[1]);
-    $tables =new $controllerName($db);
+    $tables = new $controllerName($db);
+    $tables->main();
 }else{
     //처음페이지
-    echo "처음페이지";
+    //echo "처음페이지";
     $body = file_get_contents("../Resource/index.html");
     echo $body;
 }
