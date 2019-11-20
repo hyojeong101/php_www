e48029e1e41f3780c0d5e5a9271e8c35617e5dca
hyojeong101 <aaa8132@naver.com>
2019-11-20 12:50:48 +0900

    addtable

--- Module/Database/Database.php
+++ Module/Database/Database.php
@@ -1,63 +1,61 @@
-<?php 
+<?php
 namespace Module\Database;
-//선언 -> 생성-> 호출
-//데이터 베이스 선언
+// 선언 -> 생성 -> 호출
+// 데이터베이스 선언
 class Database
 {
-    public $connect; 
-    
-    //복합개체 - 객체지향의 은닉화(숨겨놓고 꺼내먹는거)
-    //public,private,protected
+    public $connect;
+    // 복합객체 - 객체지향의 (은닉화)
+    // public, private, protected
     private $Table;
-    public function setTable($name){
+    public function setTable($name)
+    {
         $this->Table = $name;
+        
         return $this;
     }
-    public function getTable(){
+    public function getTable()
+    {
         return $this->Table;
     }
-
-
-    //생성자 메소드(함수)
+    // 생성자 메소드(함수)
     public function __construct($config)
     {
-           //테이블 객체 연결
-           $this->Table = new \Module\Database\Table($this);
-       // echo "클래스 생성";
-        // \ -> 전역 네임스페이스
-        $this->connect = new \mysqli($config['host'], $config['user'], $config['passwd'],$config['database']);
-        if(!$this->connect ->connect_errno){
-        //    echo "DB 접속 성공이에요";
-        }else{
-            echo"접속이 안돼요";
+        // 테이블 객체 연결
+        $this->Table = new \Module\Database\Table($this);
+        // echo "클래스 생성";
+        // \ == 전역 네임스페이스
+        $this->connect = new \mysqli($config['host'], $config['user'], $config['passwd'], $config['database']);
+        // 성공 : connect_errno = 0 .. ! 1
+        // 실패 : connect_errno = 1 .. ! 0
+        if (!$this->connect->connect_errno) {
+            // echo "DB 접속 성공이에요";
+        } else {
+            echo "접속이 안돼요. ㅠㅠㅠ";
         }
     }
     public function queryExecute($query)
     {
-        if($result = mysqli_query($this->connect,$query)){
-      //      echo " query성공";
-        }else {
-            print "query 실패";
+        $result = mysqli_query($this->connect, $query);
+        if ($result) {
+            // echo "쿼리 성공.";
+        } else {
+            print "쿼리 실패.";
         }
         return $result;
-
     }
-    //테이블 확인
+    // 테이블 확인
     public function isTable($tablename)
     {
-    $query = "SHOW TABLES";
-    $result = $this->queryExecute($query);
-
-    $count = mysqli_num_rows($result);
-
-    for($i=0;$i<$count;$i++){
-        $row = mysqli_fetch_object($result);
-        if($row->Tables_in_php == $tablename){
-            return true;
+        $query = "SHOW TABLES";
+        $result = $this->queryExecute($query);
+        $count = mysqli_num_rows($result);
+        for ($i=0;$i<$count;$i++) {
+            $row = mysqli_fetch_object($result);
+            if($row->Tables_in_php == $tablename) {
+                return true;
             }
-        
         }
         return false;
-
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 $config = include "../dbconf.php";
 require "../Loading.php";
+
+session_start();
 $uri = $_SERVER['REQUEST_URI'];
 $uris = explode("/",$uri);//파란책
 //print_r($uris);
@@ -12,11 +14,20 @@ if(isset($uris[1])&& $uris[1]){
    // echo $uri[1]."컨트롤러 실행";
     $controllerName = "\App\Controller\\".ucfirst($uris[1]);
     $tables = new $controllerName($db);
+    //클래스의 
     $tables->main();
 }else{
     //처음페이지
     //echo "처음페이지";
     $body = file_get_contents("../Resource/index.html");
+    if($_SESSION["email"]){
+        //로그아웃 상태
+        $body = str_replace("{{login}}","로그인 상태입니다. <a href='logout'>로그아웃</a>",$body);
+    }else{
+        //로그인 상태
+        $loginform = file_get_contents("../Resource/login.html");
+        $body = str_replace("{{login}}",$loginform,$body);
+    }
     echo $body;
 }
 // $desc = new \App\Controller\TableInfo;
